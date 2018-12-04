202efd46e6907d3b27512432dd935e66cc04dd1f
neeshashetty1105 <neesha.shetty1105@gmail.com>
2018-12-04 14:22:04 +0200

    JIRA-10: Header 1 change

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => (
     <
     header >
     <
-    h1 > Celebrity Rich List< /h1> <
+    h1 > Technical Test< /h1> <
     h2 > Celebrity Rich List < /h2> <
     p > A list of the Top 50 Richest Celebrities of 2014 < /p> <
     p > Reference: < a href = "http://www.therichest.com/top-lists/top-100-richest-celebrities/" > http: //www.therichest.com/top-lists/top-100-richest-celebrities/</a> </p>
